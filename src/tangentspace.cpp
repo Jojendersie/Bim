7e2bc5d92f5ff0dbad033c8fc11541440591bb78
Johannes Jendersie <jojendersie@web.de>
2016-11-24 12:52:46 +0100

    more robust tangent space computation (can partially ignore invalid triangles)

--- src/tangentspace.cpp
+++ src/tangentspace.cpp
@@ -42,46 +42,52 @@ namespace bim {
 			Vec3 e2 = m_positions[m_triangles[i].z] - m_positions[m_triangles[i].y];
 			Vec3 triNormal, triTangent, triBitangent;
 			triNormal = normalize(cross(e0, e1));
-			eiAssert(all(triNormal == triNormal), "NaN in normal computation!");
-			if(useTexCoords) {
-				Vec2 uva = m_texCoords0[m_triangles[i].y] - m_texCoords0[m_triangles[i].x];
-				Vec2 uvb = m_texCoords0[m_triangles[i].z] - m_texCoords0[m_triangles[i].x];
-				float det = uva.x * uvb.y - uva.y * uvb.x; // may swap the sign
-				if(det == 0.0f) det = 1.0f;
-				triTangent = (uvb.y * e0 - uva.y * e1) / det;
-				triBitangent = (uva.x * e1 - uvb.x * e0) / det;
-				// Try to recover direction if it got NaN
-				bool invalidTangent = !all(triTangent == triTangent) || len(triTangent) < 1e-10f;
-				bool invalidBitangent = !all(triBitangent == triBitangent) || len(triBitangent) < 1e-10f;
-				if(invalidTangent && invalidBitangent)
-				{
-					// Create a random orthonormal basis (no uv given)
-					triTangent = Vec3(1.0f, triNormal.x, 0.0f);
-					triBitangent = Vec3(0.0f, triNormal.z, 1.0f);
-				} else if(invalidTangent)
-					triTangent = cross(triBitangent, triNormal) * det;
-				else if(invalidBitangent)
-						triBitangent = cross(triNormal, triTangent) * det;
-				if(!ei::orthonormalize(triNormal, triTangent, triBitangent))
-					triBitangent = cross(triNormal, triTangent);
-				eiAssert(all(triTangent == triTangent), "NaN in tangent computation!");
-				eiAssert(all(triBitangent == triBitangent), "NaN in bitangent computation!");
-				eiAssert(approx(len(triTangent), 1.0f, 1e-4f), "Computed tangent has a wrong length!");
-				eiAssert(approx(len(triBitangent), 1.0f, 1e-4f), "Computed bitangent has a wrong length!");
+			// If there are invalid triangles (cause NaN) skip them
+			if(all(triNormal == triNormal))
+			{
+				if(useTexCoords) {
+					Vec2 uva = m_texCoords0[m_triangles[i].y] - m_texCoords0[m_triangles[i].x];
+					Vec2 uvb = m_texCoords0[m_triangles[i].z] - m_texCoords0[m_triangles[i].x];
+					float det = uva.x * uvb.y - uva.y * uvb.x; // may swap the sign
+					if(det == 0.0f) det = 1.0f;
+					triTangent = (uvb.y * e0 - uva.y * e1) / det;
+					triBitangent = (uva.x * e1 - uvb.x * e0) / det;
+					// Try to recover direction if it got NaN
+					bool invalidTangent = !all(triTangent == triTangent) || len(triTangent) < 1e-10f;
+					bool invalidBitangent = !all(triBitangent == triBitangent) || len(triBitangent) < 1e-10f;
+					if(invalidTangent && invalidBitangent)
+					{
+						// Create a random orthonormal basis (no uv given)
+						triTangent = Vec3(1.0f, triNormal.x, 0.0f);
+						triBitangent = Vec3(0.0f, triNormal.z, 1.0f);
+					} else if(invalidTangent)
+						triTangent = cross(triBitangent, triNormal) * det;
+					else if(invalidBitangent)
+							triBitangent = cross(triNormal, triTangent) * det;
+					if(!ei::orthonormalize(triNormal, triTangent, triBitangent))
+						triBitangent = cross(triNormal, triTangent);
+					eiAssert(all(triTangent == triTangent), "NaN in tangent computation!");
+					eiAssert(all(triBitangent == triBitangent), "NaN in bitangent computation!");
+					eiAssert(approx(len(triTangent), 1.0f, 1e-4f), "Computed tangent has a wrong length!");
+					eiAssert(approx(len(triBitangent), 1.0f, 1e-4f), "Computed bitangent has a wrong length!");
+				}
+				float lenE0 = len(e0), lenE1 = len(e1), lenE2 = len(e2);
+				float weight = acos(saturate(dot(e0, e1) / (lenE0 * lenE1)));
+				eiAssert(weight == weight, "weight is NaN");
+				if(computeNormal) m_normals[m_triangles[i].x] += triNormal * weight;
+				if(useTexCoords) m_tangents[m_triangles[i].x] += triTangent * weight;
+				if(useTexCoords) m_bitangents[m_triangles[i].x] += triBitangent * weight;
+				weight = acos(saturate(-dot(e0, e2) / (lenE0 * lenE2)));
+				eiAssert(weight == weight, "weight is NaN");
+				if(computeNormal) m_normals[m_triangles[i].y] += triNormal * weight;
+				if(useTexCoords) m_tangents[m_triangles[i].y] += triTangent * weight;
+				if(useTexCoords) m_bitangents[m_triangles[i].y] += triBitangent * weight;
+				weight = acos(saturate(dot(e1, e2) / (lenE1 * lenE2)));
+				eiAssert(weight == weight, "weight is NaN");
+				if(computeNormal) m_normals[m_triangles[i].z] += triNormal * weight;
+				if(useTexCoords) m_tangents[m_triangles[i].z] += triTangent * weight;
+				if(useTexCoords) m_bitangents[m_triangles[i].z] += triBitangent * weight;
 			}
-			float lenE0 = len(e0), lenE1 = len(e1), lenE2 = len(e2);
-			float weight = acos(saturate(dot(e0, e1) / (lenE0 * lenE1)));
-			if(computeNormal) m_normals[m_triangles[i].x] += triNormal * weight;
-			if(useTexCoords) m_tangents[m_triangles[i].x] += triTangent * weight;
-			if(useTexCoords) m_bitangents[m_triangles[i].x] += triBitangent * weight;
-			weight = acos(saturate(-dot(e0, e2) / (lenE0 * lenE2)));
-			if(computeNormal) m_normals[m_triangles[i].y] += triNormal * weight;
-			if(useTexCoords) m_tangents[m_triangles[i].y] += triTangent * weight;
-			if(useTexCoords) m_bitangents[m_triangles[i].y] += triBitangent * weight;
-			weight = acos(saturate(dot(e1, e2) / (lenE1 * lenE2)));
-			if(computeNormal) m_normals[m_triangles[i].z] += triNormal * weight;
-			if(useTexCoords) m_tangents[m_triangles[i].z] += triTangent * weight;
-			if(useTexCoords) m_bitangents[m_triangles[i].z] += triBitangent * weight;
 		}
 
 		// Orthonormalize
@@ -90,7 +96,11 @@ namespace bim {
 				ei::orthonormalize(m_normals[i], m_tangents[i], m_bitangents[i]);
 		else if(computeNormal)
 			for(size_t i = 0; i < m_normals.size(); ++i)
-				m_normals[i] = normalize(m_normals[i]);
+			{
+				float length = len(m_normals[i]);
+				if(length > 0.0f)
+					m_normals[i] /= length;
+			}
 
 		// Generate some "random" tangent spaces without the need of texture coordinates.
 		if(needsAll && !useTexCoords)
